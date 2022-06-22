19def361fccec7916866dc0bac0b0244c2ff23a2
0xArcheon <imamlan03@gmail.com>
2022-06-22 21:49:12 +0530

    added 5 rounds for a game and implemented variables to track scores

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-let playerSelection = getUserInput();
-let computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+let userScore = 0;
+let computerScore = 0;
+game();
+function game() {
+    for(let i = 0; i<5 ; i++)
+    {
+        let playerSelection = getUserInput();
+        let computerSelection = computerPlay();
+        console.log(playRound(playerSelection,computerSelection));
+        console.log("You: "+userScore+ "\nComputer: "+computerScore);
+    }
+    if(userScore > computerScore) {
+        console.log("CONGRATS! You win the game");
+    }
+    else {
+        console.log("SORRY! You lost the game");
+    }
+}
 
 function playRound(playerSelection, computerSelection) {
     switch (true) {
@@ -14,21 +29,27 @@ function playRound(playerSelection, computerSelection) {
             return `DRAW!`;
             break;
         case ((playerSelection === "Rock") && (computerSelection === "Paper")) :
+            computerScore = computerScore + 1;
             return `You LOSE! Paper beats Rock`;
             break;
         case ((playerSelection === "Rock") && (computerSelection === "Scissors")) :
+            userScore = userScore + 1;
             return `You WIN! Rock beats Scissors`;
             break;
         case ((playerSelection === "Paper") && (computerSelection === "Rock")) :
+            userScore = userScore + 1;
             return `You WIN! Paper beats Rock`;
             break;
         case ((playerSelection === "Paper") && (computerSelection === "Scissors")) :
+            computerScore = computerScore + 1;
             return `You LOSE! Scissors beat Paper`;
             break;
         case ((playerSelection === "Scissors") && (computerSelection === "Rock")) :
+            computerScore = computerScore + 1;
             return `You LOSE! Rock beats Scissors`;
             break;
         case ((playerSelection === "Scissors") && (computerSelection === "Paper")) :
+            userScore = userScore + 1;
             return `You WIN! Scissors beat Paper`;
             break;
         default : 
